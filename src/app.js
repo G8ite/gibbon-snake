7680eb7178e692d47ef4d6dad0ebbde8d3fb12bb
G8ite <amandine.pda@gmail.com>
2024-05-03 15:53:41 +0200

    init empty

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const config = {
     width: parent.innerWidth,
     height: parent.innerHeight,
     transparent: true,
-    type: Phaser.AUTO,
     physics: {
         default: 'arcade',
     },
@@ -15,33 +14,15 @@ const config = {
     }
 };
 var game = new Phaser.Game(config);
-let player;
-let cursors;
 
 function preload() {
-    this.load.image('player', 'assets/player.png');
+    
 }
 
 function create() {
-    player = this.physics.add.image(100, 100, 'player').setDisplaySize(50, 50);
-    player.setCollideWorldBounds(true);
-    cursors = this.input.keyboard.createCursorKeys();
+   
 }
 
 function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-    } 
-    else if (cursors.up.isDown) {
-        player.setVelocityY(-160);
-    }
-    else if (cursors.down.isDown) {
-        player.setVelocityY(160);
-    }
-    else {
-        player.setVelocityX(0);
-        player.setVelocityY(0);
-    }
+
 }
